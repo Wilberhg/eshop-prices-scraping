02358b865bd03ee03432ab8c314b4dd2a8e9c680
unknown <wilberhg@gmail.com>
2025-01-15 00:07:40 -0300

    style: pep8 formatation

--- main.py
+++ main.py
@@ -28,7 +28,9 @@ with httpx.Client(
     for page in range(1, 11):
         params["page"] = page
         response = client.get(f"/games/on-sale", params={**params, "page": page})
-        logging.info(f'Efetuado requisição no endpoint "{response.url}" e o código de retorno foi "{response.status_code}"')
+        logging.info(
+            f'Efetuado requisição no endpoint "{response.url}" e o código de retorno foi "{response.status_code}"'
+        )
         soup = BeautifulSoup(response.content, "html.parser")
         games_list = soup.find_all("a", class_="games-list-item")
         for game in games_list:
@@ -44,25 +46,43 @@ with httpx.Client(
                 game_score_value = game_score_element.attrs["title"]
             else:
                 game_score_value = "N/A"
-            game_infos = {"name": game_name, "price": game_price, "score": game_score_value}
+            game_infos = {
+                "name": game_name,
+                "price": game_price,
+                "score": game_score_value,
+            }
             if monetary_value <= desired_price:
                 game_page_link = game.attrs["href"]
-                game_page_response = client.get(game_page_link, params={"currency": "BRL"})
-                soup_game_page = BeautifulSoup(game_page_response.content, 'html.parser')
+                game_page_response = client.get(
+                    game_page_link, params={"currency": "BRL"}
+                )
+                soup_game_page = BeautifulSoup(
+                    game_page_response.content, "html.parser"
+                )
                 country_table_rows = soup_game_page.find_all("tr", class_="pointer")
                 for country_row in country_table_rows:
                     sale_available_until_element = country_row.find("span")
                     if sale_available_until_element:
-                        sale_available_until_value = sale_available_until_element.attrs["title"]
+                        sale_available_until_value = sale_available_until_element.attrs[
+                            "title"
+                        ]
                         break
                 sale_available_until = country_row.find("span").attrs["title"]
                 country_name = country_row.find("td", class_=None).text.strip()
-                game_infos = {**game_infos, "county": country_name, "sale": sale_available_until}
+                game_infos = {
+                    **game_infos,
+                    "county": country_name,
+                    "sale": sale_available_until,
+                }
                 game_list.append(game_infos)
                 logging.info(f'COLETADO o game "{game_infos}"')
             else:
-                logging.info(f'Jogo "{game_infos}" NÃO coletado devido ao valor de R$ {monetary_value} estar acima do esperado R$ {desired_price}')
+                logging.info(
+                    f'Jogo "{game_infos}" NÃO coletado devido ao valor de R$ {monetary_value} estar acima do esperado R$ {desired_price}'
+                )
         request_time_interval = uniform(1, 5)
-        logging.info(f'Aguardando o intervalo de {request_time_interval:.2f} segundos')
+        logging.info(f"Aguardando o intervalo de {request_time_interval:.2f} segundos")
         time.sleep(request_time_interval)
-    logging.info(f'Concluída a raspagem com um total de {len(game_list)} jogos retornados')
+    logging.info(
+        f"Concluída a raspagem com um total de {len(game_list)} jogos retornados"
+    )
